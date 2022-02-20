a1c99794e83acc53a566937968060eccac2a7774
Christoph Tack <prog.send@gmail.com>
2022-02-20 16:24:19 +0100

    Rainbox on one neopixel

--- firmware/tests/blinky/src/main.cpp
+++ firmware/tests/blinky/src/main.cpp
@@ -4,11 +4,11 @@ void setup()
 {
     // put your setup code here, to run once:
     pinMode(LED_BUILTIN, OUTPUT);
+    OSCCAL-=5;
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
     digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);
