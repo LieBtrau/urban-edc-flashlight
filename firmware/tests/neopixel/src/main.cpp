b99f41d4ccb01ff7a726c886ecbc1fc738c6eab1
Christoph Tack <prog.send@gmail.com>
2022-03-16 22:00:47 +0100

    Hue + brightness change

--- firmware/tests/neopixel/src/main.cpp
+++ firmware/tests/neopixel/src/main.cpp
@@ -8,21 +8,33 @@ Adafruit_NeoPixel pixels(1, PIN_PB1, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-	OSCCAL-=5;
-	pixels.begin(); 
-	pixels.clear(); 
+	OSCCAL -= 5;
+	pixels.begin();
+	pixels.clear();
 	Serial.begin(4800);
-    //Disable Serial RX
-    ACSR &=~(1<<ACIE);
-    ACSR |=~(1<<ACD);
+	// Disable Serial RX
+	ACSR &= ~(1 << ACIE);
+	ACSR |= ~(1 << ACD);
 	Serial.print("Hello");
+	pixels.setPixelColor(0, pixels.Color(255, 0, 0, 0));
+	pixels.show(); // Send the updated pixel colors to the hardware.
 }
 
 void loop()
 {
-	for (long i = 0; i < 65536; i+=10)
-	{ 
-		pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(i,255,64)));
-		pixels.show(); // Send the updated pixel colors to the hardware.
+	for (long hue = 0; hue < 65536; hue += 10)
+	{
+		for (int i = 0; i < 256; i++)
+		{
+			pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(hue, 255, i)));
+			pixels.show();
+			delay(10);
+		}
+		for (int i = 0; i < 256; i++)
+		{
+			pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(hue, 255, 255 - i)));
+			pixels.show();
+			delay(10);
+		}
 	}
 }
